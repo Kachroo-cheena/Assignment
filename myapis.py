db941bc1a5275d27e20a0625803f6d6f9ad63b19
Cheena Kachroo <cheenakachroo7343@gmail.com>
2022-02-27 14:59:55 +0530

    tweet complete

--- myapis.py
+++ myapis.py
@@ -4,7 +4,36 @@ import json
 import pandas as pd
 from transformers import pipeline
 from fastapi.responses import FileResponse
+
 import os
+
+
+# To set your environment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAAS%2FZgEAAAAA8y0jEu%2FyIngtc3zQXrji%2BmJnV1Y%3DkzdzjDS87FI8BE2jNGmfBquo4oyApXlRQgerwdleaJXK5JeMKR"
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {'query': 'Green Hydrogen','expansions':'author_id','tweet.fields': 'author_id,created_at,source','user.fields':'description'}
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
 app = FastAPI()
 
 @app.get("/cheena")
@@ -24,12 +53,25 @@ def index():
         url.append(m["url"])
         publish.append(m["publishedAt"])
         data = [m["title"]]
-        label.append(sentiment_pipeline(data)[0]['label'])
-        score.append(sentiment_pipeline(data)[0]['score'])
+        temp = sentiment_pipeline(data)[0]
+        label.append(temp['label'])
+        score.append(temp["score"])
         # label.append('')
         # score.append('')
 
-    t = {'Date of News':publish,'News Headline':title,'Source of News':url,"Predicted Sentimental Label":label,"Predicted Sentimental Score":score}
+    
+    json_response = connect_to_endpoint(search_url, query_params)
+    tweet_data = json_response["data"]
+    tweet_users = json_response["includes"]["users"]
+    for d,u in zip(tweet_data,tweet_users):
+        title.append(d["text"])
+        url.append(u["name"])
+        publish.append(d["created_at"])
+        temp = sentiment_pipeline(d["text"])[0]
+        label.append(temp['label'])
+        score.append(temp["score"])
+        
+    t = {'Date of News/Tweet':publish,'Tweet content/news headline':title,'source of news / person name who has tweeted':url,"Predicted Sentimental Label":label,"Predicted Sentimental Score":score}
     df = pd.DataFrame(t)
     df.to_csv("Cheena_News.csv")
     file_path = "Cheena_News.csv"
